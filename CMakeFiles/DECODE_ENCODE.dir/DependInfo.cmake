f7c935858159ec25eae57f4ae45b4a43160245ee
zzw-hcremix <3264809597@qq.com>
2018-07-27 19:39:24 +0800

    Add files via upload

---
+++ CMakeFiles/DECODE_ENCODE.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zengziwen/DECODE_ENCODE/main.cpp" "/home/zengziwen/build-DECODE_ENCODE-Desktop_Qt_5_10_1_GCC_64bit-Release/CMakeFiles/DECODE_ENCODE.dir/main.cpp.o"
+  "/home/zengziwen/DECODE_ENCODE/svjpeg.cpp" "/home/zengziwen/build-DECODE_ENCODE-Desktop_Qt_5_10_1_GCC_64bit-Release/CMakeFiles/DECODE_ENCODE.dir/svjpeg.cpp.o"
+  "/home/zengziwen/DECODE_ENCODE/svpng.cpp" "/home/zengziwen/build-DECODE_ENCODE-Desktop_Qt_5_10_1_GCC_64bit-Release/CMakeFiles/DECODE_ENCODE.dir/svpng.cpp.o"
+  "/home/zengziwen/DECODE_ENCODE/toolbox.cpp" "/home/zengziwen/build-DECODE_ENCODE-Desktop_Qt_5_10_1_GCC_64bit-Release/CMakeFiles/DECODE_ENCODE.dir/toolbox.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "OPENCV_TRAITS_ENABLE_DEPRECATED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/zengziwen/DECODE_ENCODE"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/DECODE_ENCODE.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DECODE_ENCODE.dir/main.cpp.o"
+  "CMakeFiles/DECODE_ENCODE.dir/toolbox.cpp.o"
+  "CMakeFiles/DECODE_ENCODE.dir/svjpeg.cpp.o"
+  "CMakeFiles/DECODE_ENCODE.dir/svpng.cpp.o"
+  "DECODE_ENCODE.pdb"
+  "DECODE_ENCODE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DECODE_ENCODE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
